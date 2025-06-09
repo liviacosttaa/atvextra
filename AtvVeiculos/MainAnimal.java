b78021f67db571fdfbee778a0f94b69b089b4029
liviacosttaa <analiviasas@gmail.com>
2025-06-09 20:03:13 -0300

    classe main animal

--- AtvVeiculos/MainAnimal.java
+++ AtvVeiculos/MainAnimal.java
@@ -1,5 +1,18 @@
 package AtvVeiculos;
 
 public class MainAnimal {
-    
+    public static void main(String[] args) {
+        
+        Animal leao = new Leao();
+        System.out.println("Leão:");
+        leao.emitirSom();
+        leao.alimentar();
+        System.out.println();
+       
+        Animal peixe = new Peixe();
+        System.out.println("Peixe:");
+        peixe.emitirSom();
+        peixe.alimentar();
+        ((Peixe) peixe).nadar(); 
+    }
 }
