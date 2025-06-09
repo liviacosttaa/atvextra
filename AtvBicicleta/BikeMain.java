843dfe9356d1642d4d078feee27146a0bba51581
liviacosttaa <analiviasas@gmail.com>
2025-06-09 12:10:02 -0300

    classe principal

---
+++ AtvBicicleta/BikeMain.java
@@ -0,0 +1,21 @@
+public class Bicicleta {
+    private int velocidade;
+    private int marcha;
+    
+    public Bicicleta() {
+        this.velocidade = 0;
+        this.marcha = 0;
+    }
+    public void acelerar(int incremento) {
+        if (incremento > 0) {
+            this.velocidade += incremento;
+        }
+    }
+    public void frear(int decremento) {
+        if (decremento > 0) {
+            this.velocidade -= decremento;
+            if (this.velocidade < 0) {
+                this.velocidade = 0;
+            }
+        }
+    }
