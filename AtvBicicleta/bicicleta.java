9b2e586b5823d64a5e317935ad2543db18e3a95a
liviacosttaa <analiviasas@gmail.com>
2025-06-09 12:01:12 -0300

    metodos

--- AtvBicicleta/bicicleta.java
+++ AtvBicicleta/bicicleta.java
@@ -4,4 +4,20 @@ public class Bicicleta {
    
     public Bicicleta() {
         this.velocidade = 0; 
-        this.marcha = 0;     
+        this.marcha = 0;
+         }
+
+    public void acelerar(int incremento) {
+        if (incremento > 0) {
+            this.velocidade += incremento;
+        }
+    }
+    
+    public void frear(int decremento) {
+        if (decremento > 0) {
+            this.velocidade -= decremento;
+            if (this.velocidade < 0) {
+                this.velocidade = 0; 
+            }
+        }
+    }
