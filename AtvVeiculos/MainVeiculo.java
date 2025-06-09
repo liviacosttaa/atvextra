ec6378ee4295f970472ce1e777454bc1fd3ae9ce
liviacosttaa <analiviasas@gmail.com>
2025-06-09 20:19:26 -0300

    classe moto e bic, correção de erros

---
+++ AtvVeiculos/MainVeiculo.java
@@ -0,0 +1,5 @@
+package AtvVeiculos;
+
+public class MainVeiculo {
+    
+}
--- AtvVeiculos/Veiculo.java
+++ AtvVeiculos/Veiculo.java
@@ -3,14 +3,18 @@ package AtvVeiculos;
 abstract class Veiculo {
     protected String modelo;
     protected int ano;
+
     public Veiculo(String modelo, int ano) {
         this.modelo = modelo;
         this.ano = ano;
     }
+
     public abstract void alugar();
+
     public String getModelo() {
         return modelo;
     }
+
     public int getAno() {
         return ano;
     }
@@ -19,30 +23,48 @@ abstract class Veiculo {
 interface Motorizado {
     void abastecer();
 }
+
 class Carro extends Veiculo implements Motorizado {
     public Carro(String modelo, int ano) {
         super(modelo, ano);
     }
+
     @Override
     public void alugar() {
         System.out.println("Carro " + modelo + " do ano " + ano + " alugado.");
     }
+
     @Override
     public void abastecer() {
         System.out.println("Abastecendo o carro " + modelo + ".");
     }
 }
 
+
 class Moto extends Veiculo implements Motorizado {
     public Moto(String modelo, int ano) {
         super(modelo, ano);
     }
+
     @Override
     public void alugar() {
         System.out.println("Moto " + modelo + " do ano " + ano + " alugada.");
     }
+
     @Override
     public void abastecer() {
         System.out.println("Abastecendo a moto " + modelo + ".");
+    }
+}
+
+
+class BicicletaEletrica extends Veiculo {
+    public BicicletaEletrica(String modelo, int ano) {
+        super(modelo, ano);
+    }
+
+    @Override
+    public void alugar() {
+        System.out.println("Bicicleta elétrica " + modelo + " do ano " + ano + " alugada.");
     }
 }
