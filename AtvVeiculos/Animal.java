c11391ae53ac721e1fe724d83b8dc2ee7dc7ffe8
liviacosttaa <analiviasas@gmail.com>
2025-06-09 20:01:22 -0300

    classe leao,peixe e main

--- AtvVeiculos/Animal.java
+++ AtvVeiculos/Animal.java
@@ -8,3 +8,29 @@ abstract class Animal {
 interface Aquatico {
     void nadar();
 }
+
+class Leao extends Animal {
+    @Override
+    public void emitirSom() {
+        System.out.println("O leão ruge!");
+    }
+    @Override
+    public void alimentar() {
+        System.out.println("O leão está se alimentando de carne.");
+    }
+}
+
+class Peixe extends Animal implements Aquatico {
+    @Override
+    public void emitirSom() {
+        System.out.println("O peixe faz bolhas!");
+    }
+    @Override
+    public void alimentar() {
+        System.out.println("O peixe está se alimentando de flocos.");
+    }
+    @Override
+    public void nadar() {
+        System.out.println("O peixe está nadando.");
+    }
+}
